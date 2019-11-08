836b5dc8fd779a3f5d0d9e63abed65790cc665c4
rathorerahul586 <32606526+rathorerahul586@users.noreply.github.com>
2019-11-08 21:44:25 +0530

    2.4 Rows

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,23 +1,34 @@
-import 'dart:ui' as prefix0;
-
 import "package:flutter/material.dart";
 
-class MyFirstFlatter extends StatelessWidget{
+class MyFirstFlatter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Center(
       child: Container(
         alignment: Alignment.center,
-        child: (
-            Text("Hello",
-                style: TextStyle(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+                child: Text(
+              "Hello World",
+              style: TextStyle(
                   decoration: TextDecoration.none,
                   color: Colors.white,
                   fontFamily: "ZCOOLXiaoWei",
                   fontSize: 60.0,
-                  fontWeight: FontWeight.w300
-                ),)
+                  fontWeight: FontWeight.w300),
+            )),
+            Expanded(
+                child: Text(
+              "This is long text",
+              style: TextStyle(
+                  decoration: TextDecoration.none,
+                  color: Colors.white,
+                  fontFamily: "ZCOOLXiaoWei",
+                  fontSize: 60.0,
+                  fontWeight: FontWeight.w300),
+            )),
+          ],
         ),
       ),
     );
