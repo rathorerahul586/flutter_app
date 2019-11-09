d37dfa4bfcbf711274f6129aa58f8c61c66a9ae1
rathorerahul586 <32606526+rathorerahul586@users.noreply.github.com>
2019-11-09 10:04:04 +0530

    2.6 Button with on press and Alert Dialog

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -5,6 +5,7 @@ class MyFirstFlatter extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
+        color: Colors.green,
         padding: EdgeInsets.only(top: 40.0, left: 20.0),
         child: Column(
           children: <Widget>[
@@ -58,20 +59,49 @@ class MyFirstFlatter extends StatelessWidget {
                     ))
               ],
             ),
-            flightImage()
+            flightImage(),
+            bookingBtn(context),
           ],
         ),
       ),
     );
   }
-}
-
-class flightImage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  Container flightImage(){
     AssetImage assetImage = AssetImage('images/flight.png');
     Image flightTakeOff = Image(image: assetImage, width: 300, height: 200,);
-    return Container(child: flightTakeOff,);
+    return Container(child: flightTakeOff);
   }
 
+  Container bookingBtn(BuildContext context){
+    return Container(
+      width: 300.0,
+      height: 50.0,
+      child: RaisedButton(
+        color: Colors.deepOrange,
+        elevation: 6.0,
+        child: Text(
+          "Book Flight",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+            fontFamily: 'ZCOOLXiaoWei'
+          ),
+        ),
+        onPressed: (){
+            bookFlight(context);
+        },
+      ),
+    );
+  }
+
+  void bookFlight(BuildContext context){
+    var alertDilog = AlertDialog(
+      title: Text("Flight Booked Successgully"),
+      content: Text("Have A Nice Day. Happy Journy"),
+    );
+    showDialog(context: context,
+    builder: (BuildContext context){
+      return alertDilog;
+    });
+  }
 }
