bd4d247801d1342dde80d95ea1758aa6c01e78f6
Rahul Kumar <rathorerahul586@gmail.com>
2020-09-21 18:59:01 +0530

    Simple Todo App

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -6,34 +6,81 @@ class MyFlutterApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "First Flutter",
-      home: Scaffold(
-        appBar: AppBar (
-          title: (Text("Good Rahul")),
-        ),
-        body: Material(
-          color: Colors.green,
-          child: Center (
-            child: Text(
-              "Hello Rahul"+luckyNumber(),
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-          ),
-        ),
-      ),
+    return new MaterialApp(
+      title: 'Todo List',
+      home: new TodoList(),
     );
   }
 
-  String luckyNumber(){
+  String luckyNumber() {
     var random = Random();
     int randomNumber = random.nextInt(10);
 
     return "\nYour Lucky No. is $randomNumber";
+  }
+}
+
+class TodoList extends StatefulWidget {
+  @override
+  createState() => new TodoListState();
+}
+
+class TodoListState extends State<TodoList> {
+  List<String> _todoItem = [];
+
+  void _addTodoItem(String todoItem) {
+    setState(() {
+      int index = _todoItem.length;
+      _todoItem.add(todoItem);
+    });
+  }
+
+  Widget _buildTodoList() {
+    return new ListView.builder(itemBuilder: (context, index) {
+      if (index < _todoItem.length) {
+        return _buildTodoItem(_todoItem[index]);
+      }
+    });
+  }
+
+  // Build a single todo item
+  Widget _buildTodoItem(String todoText) {
+    return new ListTile(title: new Text(todoText));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todo List'),
+      ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _pushAddTodoScreen,
+        tooltip: 'Add Task',
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _pushAddTodoScreen() {
+    // Push this page onto the stack
+    Navigator.of(this.context).push(
+        // MaterialPageRoute will automatically animate the screen entry, as well
+        // as adding a back button to close it
+        new MaterialPageRoute(builder: (context) {
+      return new Scaffold(
+          appBar: new AppBar(title: new Text('Add a new task')),
+          body: new TextField(
+            autofocus: true,
+            onSubmitted: (val) {
+              _addTodoItem(val);
+              Navigator.pop(context); // Close the add todo screen
+            },
+            decoration: new InputDecoration(
+                hintText: 'Enter something to do...',
+                contentPadding: const EdgeInsets.all(16.0)),
+          ));
+    }));
   }
 }
